5887fc4f6878dede1613c74efe18f2e3fbeb7a0a
Tfinch <travisfinch01@gmail.com>
2020-06-12 23:34:50 -0400

    Test Check All

--- commands/checkAll.js
+++ commands/checkAll.js
@@ -1,158 +1,17 @@
 const { MessageEmbed, splitMessage } = require("discord.js");
+const MI = require('./checkMI.js');
+const IL = require('./checkIL.js');
 
 module.exports = {
     name: "startall",
     aliases: ['all'],
     description: "Check all warehouse folders.",
-    execute(message) {
+    execute() {
 
-        // Imports / Requires
-        var dirwatch = require("./DirectoryWatcher.js");
-
-        // Create a monitor object that will watch a directory
-        // and all it's sub-directories (recursive) in this case
-        // we'll assume you're on a windows machine with a folder 
-        // named "sim" on your c: drive.
-        // should work on both linux and windows, update the path
-        // to some appropriate test directory of your own.
-        // you can monitor only a single folder and none of its child
-        // directories by simply changing the recursive parameter to
-        // to false
-        var allMonitor = new dirwatch.DirectoryWatcher("Z:", true);
-
-        // start the monitor and have it check for updates
-        // every half second.
-        allMonitor.start(60000);
-
-        // Log to the console when a file is removed
-        allMonitor.on("fileRemoved", function (filePath) {
-            message.channel.send({
-                embed: {
-                    color: 0x2ecc71,
-                    title: "PickTicket Deleted",
-                    fields: [{
-                        name: "PickTicket:",
-                        value: filePath
-                    }
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        text: "Current Time"
-                    }
-                }
-            });
-            console.log("File Deleted: " + filePath);
-        });
-
-        // Log to the console when a folder is removed
-        allMonitor.on("folderRemoved", function (folderPath) {
-            message.channel.send({
-                embed: {
-                    color: 0x2ecc71,
-                    title: "Folder Deleted",
-                    fields: [{
-                        name: "PickTicket Folder:",
-                        value: folderPath
-                    }
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        text: "Current Time"
-                    }
-                }
-            });
-            console.log("Folder Removed: " + folderPath);
-        });
-
-        // log to the console when a folder is added
-        allMonitor.on("folderAdded", function (folderPath) {
-            message.channel.send({
-                embed: {
-                    color: 0x2ecc71,
-                    title: "Folder Added",
-                    fields: [{
-                        name: "PickTicket Folder:",
-                        value: folderPath
-                    }
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        text: "Current Time"
-                    }
-                }
-            });
-            console.log(folderPath);
-        });
-
-        // Log to the console when a file is changed.
-        allMonitor.on("fileChanged", function (fileDetail, changes) {
-            for (var key in changes) {
-                message.channel.send({
-                    embed: {
-                        color: 0x2ecc71,
-                        title: "File Changed",
-                        description: "  + " + key + " changed...",
-                        fields: [{
-                            name: "    - From: ",
-                            value: ((changes[key].baseValue instanceof Date) ? changes[key].baseValue.toISOString() : changes[key].baseValue)
-                        },
-                        {
-                            name: "    - To  : ",
-                            value: ((changes[key].comparedValue instanceof Date) ? changes[key].comparedValue.toISOString() : changes[key].comparedValue)
-                        }
-                        ],
-                        timestamp: new Date(),
-                        footer: {
-                            text: "Current Time"
-                        }
-                    }
-                });
-                console.log("File Changed: " + fileDetail.fullPath);
-                console.log("  + " + key + " changed...");
-                console.log("    - From: " + ((changes[key].baseValue instanceof Date) ? changes[key].baseValue.toISOString() : changes[key].baseValue));
-                console.log("    - To  : " + ((changes[key].comparedValue instanceof Date) ? changes[key].comparedValue.toISOString() : changes[key].comparedValue));
-            }
-
-        });
-
-        // log to the console when a file is added.
-        allMonitor.on("fileAdded", function (fileDetail) {            
-            message.channel.send({
-                embed: {
-                    color: 0x2ecc71,
-                    title: "New PickTicket",
-                    fields: [{
-                        name: "PickTicket:",
-                        value: fileDetail.fullPath
-                    }
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        text: "Current Time Status"
-                    }
-                }
-            });
-            console.log("File Added: " + fileDetail.fullPath);
-        });
-
-        message.channel.send({
-            embed: {
-                color: 0x2ecc71,
-                title: "Monitoring All Warehouse Location Folders!",
-                fields: [{
-                    name: "Monitoring mapped drive below!:",
-                    value: allMonitor.root
-                }
-                ],
-                timestamp: new Date(),
-                footer: {
-                    text: "Current Time Status"
-                }
-            }
-        });
-        // Let us know that directory monitoring is happening and where.
-        console.log("Directory Monitoring of " + allMonitor.root + " has started");
-
+        MI;
+        console.log("Started Watching MI Folder");
+        IL;
+        console.log("Started Watching IL Folder");
     }
 };
 
