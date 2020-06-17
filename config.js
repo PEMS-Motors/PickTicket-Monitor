8630cbbfc9eef83e8ee2bc93ce64bffea2ec071d
Tfinch <travisfinch01@gmail.com>
2020-06-16 22:37:53 -0400

    add colors

--- config.js
+++ config.js
@@ -58,6 +58,25 @@ module.exports = {
         "mostrecent": "720455882332438600",
         "deleted": "720455909481906176",
         "recentlychanged": "720456429592641557",
-        "status": "722104651297587261"
+        "status": "722104651297587261",
+        "botspam": "664878346575806490"
+    },
+    "colors": {
+        "purple_dark": "#6a006a",
+        "purple_medium": "#a958a5",
+        "purple_light": "#c481fb",
+        "orange": "#ffa500",
+        "gold": "#daa520",
+        "red_dark": "#8e2430",
+        "red_light": "#f94343",
+        "blue_dark": "#3b5998",
+        "cyan": "#5780cd",
+        "blue_light": "#ace9e7",
+        "aqua": "#33a1ee",
+        "pink": "#ff9dbb",
+        "green_dark": "#2ac075",
+        "green_light": "#a1ee33",
+        "white": "#f9f9f6",
+        "cream": "#ffdab9"
     }
 }
